0eaf3de0f2b88655faaf1a5e9449fe7a13a8cbba
zerotransit <zerotransit@gmail.com>
2018-07-02 13:11:08 +0530

    Initial commit

---
+++ app/src/main/java/com/bluetooth/myapplicationui/Main2Activity.java
@@ -0,0 +1,28 @@
+package com.bluetooth.myapplicationui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain3Activity();
+            }
+        });
+    }
+    public void openMain3Activity(){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
